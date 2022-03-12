cd4f109acee88a4d1d28b2d4210679282bcb8495
Menna Hamdy <menna.h.m.elsayed@gmail.com>
2022-03-12 18:51:14 +0200

    added day 3 tasks

--- increasedArray.py
+++
@@ -1,7 +0,0 @@
-def incArrayfn(length,start):
-    incArray=[]
-    for i in range(length):
-        incArray.append(start)
-        start+=1
-    return incArray
-print(incArrayfn(5,3))
--- reverseString.py
+++
@@ -1,4 +0,0 @@
-def reverseString():
-    string = input("please enter a string")
-    return string[::-1]
-print(reverseString())
